a2c1c53df9910427a0048023882fb4d208ddaf06
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-06-03 12:43:20 +0530

    fix: typo error

--- server/index.js
+++ server/index.js
@@ -21,7 +21,17 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173'].filter(Boolean);
+app.use(cors({ 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(cookieParser());
 
